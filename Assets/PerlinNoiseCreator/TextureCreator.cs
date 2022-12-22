8048063938d8e9c5d458feaae12f27576495cde9
Chihiko <89134046+Chihiko3@users.noreply.github.com>
2022-12-23 01:54:09 +0800

    PerlinNoiseCreator_unfinished

--- Assets/PerlinNoiseCreator/TextureCreator.cs
+++ Assets/PerlinNoiseCreator/TextureCreator.cs
@@ -1,33 +1,64 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TextureCreator : MonoBehaviour
 {
+
+    [Range(1, 3)] 
+    public int dimentions = 3;
+    
     [Range(2,512)]
     public int resolution = 256;
     private Texture2D texture;
 
     private void OnEnable()
     {
-        texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
-        texture.name = "Procedural Texture";
-        texture.wrapMode = TextureWrapMode.Clamp;
-        texture.filterMode = FilterMode.Bilinear;
-        GetComponent<MeshRenderer>().material.mainTexture = texture;
+        if (texture == null)
+        {
+            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
+            texture.name = "Procedural Texture";
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = FilterMode.Trilinear;
+            texture.anisoLevel = 9;
+            GetComponent<MeshRenderer>().material.mainTexture = texture;
+        }
         FillTexture();
     }
 
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            transform.hasChanged = false;
+            FillTexture();
+        }
+    }
+
+    public float frequency = 1f;
     public void FillTexture()
     {
         if (texture.width != resolution)
         {
             texture.Reinitialize(resolution, resolution);
         }
+
+        Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
+        Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
+        Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+        Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
+
+        NoiseMethod method = Noise.valueMethods[dimentions - 1];
         float stepSize = 1f / resolution;
         for (int y = 0; y < resolution; y++)
         {
+            Vector3 point0 = Vector3.Lerp(point00, point01, (y+0.5f) * stepSize);
+            Vector3 point1 = Vector3.Lerp(point10,point11,(y+0.5f) * stepSize);
             for (int x = 0; x < resolution; x++)
             {
-                texture.SetPixel(x,y,new Color((x+0.5f) * stepSize, (y+0.5f) *stepSize,0f)); // UV Coordinates
+                Vector3 point = Vector3.Lerp(point0,point1,(x+0.5f)*stepSize);
+                texture.SetPixel(x,y,Color.white * method(point,frequency));
             }
         }
         texture.Apply();
